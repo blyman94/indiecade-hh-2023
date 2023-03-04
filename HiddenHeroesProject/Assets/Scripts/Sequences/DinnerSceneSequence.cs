85bf0593ca979587788087c8022b3b13d29ff608
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2023-03-04 14:44:16 -0500

    Sequence update

--- HiddenHeroesProject/Assets/Scripts/Sequences/DinnerSceneSequence.cs
+++ HiddenHeroesProject/Assets/Scripts/Sequences/DinnerSceneSequence.cs
@@ -15,10 +15,13 @@ public class DinnerSceneSequence : MonoBehaviour
     public float timeBeforeButton = 2.0f;
     public float timeBeforeNextPassage = 1.0f;
     public bool AwaitingPlayerInput { get; set; } = false;
+    public float cameraShakeTime = 5.0f;
+    public UIImageShake backgroundShaker;
 
     private void Start()
     {
         StartCoroutine(DinnerSequenceRoutine());
+        backgroundShaker.shakeDuration = cameraShakeTime;
     }
 
     private IEnumerator DinnerSequenceRoutine()
@@ -40,5 +43,29 @@ public class DinnerSceneSequence : MonoBehaviour
                 yield return null;
             }
         }
+
+        NextButtonAnimator.Play("NextButton_Idle");
+        TextPopupAnimator.SetTrigger("Reset");
+        backgroundShaker.StartShake();
+        yield return new WaitForSeconds(cameraShakeTime);
+
+        foreach (string passage in PassagesAfter)
+        {
+            NextButtonAnimator.Play("NextButton_Idle");
+            TextPopupAnimator.SetTrigger("Reset");
+            yield return new WaitForSeconds(timeBeforeNextPassage);
+            CurrentDialogueString.Value = passage;
+            TextPopupAnimator.SetTrigger("FloatUp");
+            yield return new WaitForSeconds(timeBeforeButton);
+            NextButtonAnimator.Play("NextButton_FloatUp");
+            AwaitingPlayerInput = true;
+            while (AwaitingPlayerInput)
+            {
+                yield return null;
+            }
+        }
+
+        NextButtonAnimator.Play("NextButton_Idle");
+        TextPopupAnimator.SetTrigger("Reset");
     }
 }
