e49d9ac6c91d85641d6af80558fefeb80070e94a
BleuSky03 <taksin@photomann.com>
2023-03-04 03:42:44 -0500

    base functionality

---
+++ HiddenHeroesProject/Assets/Scripts/BikeGame/BikeDriving.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeDriving : MonoBehaviour
+{
+    [SerializeField]
+    float maxVerticalSpeed = 5f;
+    [SerializeField]
+    float verticalSpeed = 50f;
+    [SerializeField]
+    float verticalDampening = 1f;
+    private float verticalInput;
+
+    [SerializeField]
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePositionZ;
+    }
+
+    private void FixedUpdate()
+    {
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        float move = verticalInput * verticalSpeed * Time.fixedDeltaTime;
+        Vector3 moveDirection = new Vector3(0f, move, 0f);
+        //rb.MovePosition(rb.position + moveDirection);
+        rb.AddForce(moveDirection, ForceMode.Force);
+        if (rb.velocity.magnitude > maxVerticalSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxVerticalSpeed;
+        }
+
+        if (Mathf.Approximately(verticalInput, 0f))
+        {
+            float currentVelocity = rb.velocity.y;
+            float newVelocity = Mathf.Lerp(currentVelocity, 0f, Time.fixedDeltaTime * verticalDampening);
+            rb.velocity = new Vector3(rb.velocity.x, newVelocity, rb.velocity.z);
+        }
+    }
+
+    public void slowDown()
+    {
+
+    }
+}
