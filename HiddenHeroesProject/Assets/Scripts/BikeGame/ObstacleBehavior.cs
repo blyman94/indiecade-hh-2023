ada7e2e02863d57c1c92703fc68bdce32baa0e33
BleuSky03 <taksin@photomann.com>
2023-03-04 03:42:44 -0500

    base functionality

--- HiddenHeroesProject/Assets/Scripts/BikeGame/ObstacleBehavior.cs
+++ HiddenHeroesProject/Assets/Scripts/BikeGame/ObstacleBehavior.cs
@@ -15,23 +15,6 @@ public class ObstacleBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 speed = new Vector2(-BikeGameManager.managerSpeed * Time.deltaTime * 3.3f, 0);
-        transform.Translate(speed, Space.Self);
-        if (Mathf.Abs(transform.position.x) > Mathf.Abs(BikeGameManager.minX))
-        {
-            Destroy(gameObject);
-        }
+        
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("obstacle triggered");
-        if (other.CompareTag("Player"))
-        {
-            var player = other.GetComponent<BikeDriving>();
-            player.slowDown(slowdownAmount);
-
-        }
-    }
-   
 }
