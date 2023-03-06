824e541287e8fa62576684fb7b3b48f25dd47ae8
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2023-03-05 20:04:36 -0500

    Add cone asset

--- HiddenHeroesProject/Assets/Scripts/BikeGame/ObstacleSpawner.cs
+++ HiddenHeroesProject/Assets/Scripts/BikeGame/ObstacleSpawner.cs
@@ -31,7 +31,20 @@ public class ObstacleSpawner : MonoBehaviour
         while (!BikeGameManager.isGameOver) 
         {
             Debug.Log("spawned obstacle");
-            obstacleToSpawn = Mathf.RoundToInt(Random.Range(0, obstacles.Length - 1));
+            /*
+                You originally had:
+                     obstacleToSpawn = Mathf.RoundToInt(Random.Range(0, obstacles.Length - 1));
+
+                Couple of things: The Random.Range(int, int) function will always
+                return an integer. No need to round it! In fact, doing so hid a
+                bug...
+
+                Random.Range(int, int)'s second argument is exclusive. So
+                Random.Range(0,1) can only return 0. Random.Range(0,2) can
+                return 0 and 1. Therefore, you don't need to subtract 1 from the
+                obstacle length to access all in the array.
+            */
+            obstacleToSpawn = Random.Range(0, obstacles.Length);
             yield return new WaitForSeconds(BikeGameManager.managerSpawnrate * Random.Range(0.5f, 1.5f) * 4);
             spawnLocation = Random.Range(-spawnerRadius, spawnerRadius);
             Instantiate(obstacles[obstacleToSpawn], new Vector3(transform.position.x, transform.position.y + spawnLocation, 0), transform.rotation);
